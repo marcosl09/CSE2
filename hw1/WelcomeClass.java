920f65da3e6385d66417fa26f2006edb4455e0e0
mal219 <mal219@lehigh.edu>
2015-08-31 02:47:06 +0000

    homework 1

---
+++ hw1/WelcomeClass.java
@@ -0,0 +1,20 @@
+////////////
+//Marcos Lozano Mendez
+
+
+//class
+public class WelcomeClass{
+    //main method
+    public static void main(String[] args){
+        
+        System.out.println(" ----------------");
+        System.out.println(" |  Welcome     | ");
+        System.out.println(" ----------------");
+        System.out.println(" ^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\/");
+        System.out.println("|-M--A--L--2--1--9-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+    }
+    
+}
---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////
+//Marcos Lozano Mendez
+//Hello World Java Program
+
+//class
+public class HelloWorld{
+    //main method
+    public static void Main(String[] args){
+        //print statement
+        System.out.println("##################################");
+        System.out.println("##################################");
+        System.out.println("#                                #");
+        System.out.println("#       Hello World 2015         #");
+        System.out.println("#                                #");
+        System.out.println("##################################");
+        System.out.println("##################################");
+        System.out.println("test");
+        //test
+    }
+}
