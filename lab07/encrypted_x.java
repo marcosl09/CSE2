afb3a0704f895decf6af26da45156334d98bd30d
mal219 <mal219@lehigh.edu>
2015-10-08 17:16:34 +0000

    lab07 comments added 10/8

--- lab07/encrypted_x.java
+++ lab07/encrypted_x.java
@@ -3,34 +3,34 @@
 //October 8, 2015
 //Encrypted X: hides an x inside a block of stars
 
-import java.util.Scanner;
+import java.util.Scanner;//imported the Scanner class
 
-public class encrypted_x{
-    public static void main(String[] args){
-        Scanner Al = new Scanner(System.in);
+public class encrypted_x{//start of class
+    public static void main(String[] args){//start of main method
+        Scanner Al = new Scanner(System.in);//initializes the Scanner class
         
-        int p = 0;
+        int p = 0;//stores the number that will make the x bigger
         
-        System.out.print("How big should the x be? ");
-        p = Al.nextInt();
+        System.out.print("How big should the x be? ");//ask the user for input
+        p = Al.nextInt();//stores the input in the variable
         
-        if(p > 100 || p < 0){
-            System.exit(-1);
-        }
+        if(p > 100 || p < 0){//runs when the number is above 100 and below 0
+            System.exit(-1);//exits the program
+        }//end of if
         else{
-            for(int i = 0; i < p; i++){
+            for(int i = 0; i < p; i++){//makes the rows
                 for(int j = 0; j < p; j++){
-                    if( j == i || j == (p-i)){
-                        System.out.print(" ");
-                    }
+                    if( j == i || j == (p-i)){//there is a space needed a certain amount of spaces
+                        System.out.print(" ");//puts a space
+                    }//end of if
                     else{
-                      System.out.print("*");  
-                    }
+                      System.out.print("*");  //puts the star in the row
+                    }//end of else
                     
-                }
-                System.out.println();
-            }
-        }
+                }//end of for loop with variable j
+                System.out.println();//goes to new line
+            }//end of for loop with variable i
+        }//end of else
         
-    }
-}
+    }//end of main method
+}//end of class
